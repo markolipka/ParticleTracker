bbf6d07449ac3db67bb5d935e104930ce2917067
Marko Lipka <marko.lipka@posteo.de>
2018-02-28 14:26:46 +0100

    added comments

--- ParticleTracker.R
+++ ParticleTracker.R
@@ -1,41 +1,51 @@
-
+## Helper functions for particle tracking
+## - extract EXIF data from raster images of sinking particles
+## - easily measure particle positions in image stacks
 
 #FIXME: resizing of the plot window before locate()ing points leads to wrong results!
 
-library(raster)
-library(exif)
+library(exif) # for extraction of EXIF data from image files
+library(raster) # for import and processing of raster images
 
+# Extracting timestamps from a set of image files of given file extension in a given directory
 timestamp.from.images <- function(path = "test/FakeParticles/",
-                                  format = "jpg",
-                                  exif.attribute = "origin_timestamp"){
-    images <- list.files(path, full.names = T,
-                         include.dirs = F, all.files = F,
-                         pattern = format)
-    num.images = length(images)
+                                  format = "jpg", # pattern for file matching
+                                  exif.attribute = "origin_timestamp"){ # name of exif attribute to extract
+    images <- list.files(path, full.names = T, # get a list of images with their full path in the path ...
+                         include.dirs = F, all.files = F, # ... without hidden files and directories ...
+                         pattern = format) # ... matching a given pattern
+    num.images = length(images) # number of files
     if (num.images < 2) stop("Number of images must be at least 2")
     
-    df <- do.call(rbind,
-                  lapply(images,  FUN = function(file){
-                      data.frame("filename" = file, 
-                                 "timestamp" = strptime(read_exif(file)[, exif.attribute],
+    # TODO: if is.na(exif.attribute) PRINT LIST OF ATTRIBUTES IN THE FILES
+    df <- do.call(rbind, # combine into a data.frame the output of the loop:
+                  # get EXIF data from every file
+                  lapply(images,  FUN = function(file){ 
+                      data.frame("filename" = file,  # filename in df for later merging!
+                                 "timestamp" = strptime(read_exif(file)[, exif.attribute], 
                                                         format = "%Y:%m:%d %H:%M:%S", tz = "UTC"))
                   }))
+    
+    # Extract min and max time and their diff
     min <- min(df$timestamp)
     max <- max(df$timestamp)
     diff.s <- difftime(max, min, units = "sec")
+    
     return(list(data = df, min = min, max = max, diff.s = diff.s))
 }
 
+# Track particle positions in composite image from stack of input files
 particle.positions.from.images <- function(path = "test/FakeParticles/",
                                            format = "jpg",
-                                           firstlast = TRUE,
-                                           sep.window = TRUE,
-                                           output.fig = TRUE){
+                                           firstlast = TRUE, # process only first and last image
+                                           sep.window = TRUE, # open separate window for particle localization
+                                           output.fig = TRUE){ # TODO: implement output of the resulting figure
     message(paste("Reading path ", path, " ..."))
     if (!file.exists(path)) stop("Path does not exist...")
     images <- list.files(path, full.names = T,
                          include.dirs = F, all.files = F,
                          pattern = format)
+    print(images)
     num.images = length(images)
     if (num.images < 2) stop("Number of images must be at least 2")
     
@@ -44,43 +54,49 @@ particle.positions.from.images <- function(path = "test/FakeParticles/",
         num.images <- 2
     }
     
-
-    rasters <- lapply(images, function(img) raster(img))
-    dimensions <- mapply(dim, rasters)
-
+    
+    rasters <- lapply(images, function(img) raster(img)) # Read image files and store as list of raster objects
+    dimensions <- mapply(dim, rasters) # dimensions of all raster objects for comparison
+    
+    # store information of raster objects in data frame
     df <- data.frame("filename" = images,
                      "width" = dimensions[2,],
                      "height" = dimensions[1,])
     
+    # Warn, if dimensions of the images are not uniform
     if (nrow(unique(df[, c("width", "height")])) > 1) {
         print(df)
         warning("Dimensions of images are not uniform")
     }
     
-    composed <- min(stack(rasters))
+    # compose raster objects
+    composed <- min(stack(rasters)) # darker colours have lower values, hence, the darkest pixels of al raster layers are kept in the composite layer
     
-    #if (output.fig) png(filename = "ParticleTrack.png")
+    # Particle Tracking:
     message(paste(num.images, "images loaded and compiled.\n Please klick at\n",
-                         num.images, "particles (top to bottom)"))
+                  num.images, "particles (top to bottom)"))
     
-    if (sep.window) x11(bg = "pink", height = 10, width = 10)
-    par(mar = c(0,2.5,0,0))
+    if (sep.window) x11(bg = "pink", height = 10, width = 10) # Option: separate window
+    par(mar = c(0,2.5,0,0)) # get rid of useless margins
     plot(composed,
          legend = TRUE,
          #axes = FALSE,
          col = grey(seq(0, 1, length = 256))
-         )
+    )
     
-    locs <- as.data.frame(locator(n = num.images, type = "o"))
+    locs <- as.data.frame(locator(n = num.images, # store coordinates of particles (one per image) in data.frame
+                                  type = "o")) # and highlight marked coordinates
     
-    text(x = mean(locs$x) * 1.25,
-         y = locs$y,
+    # label marked points with y-coordinates
+    text(x = mean(locs$x) * 1.25, 
+         y = locs$y, # 
          labels = round(locs, digits = 2)[, 2])
     
     #dev.off()
     return(df = cbind(df, locs))
 }
 
+# combine both functions to return data frame (and later plot ??) for each directory in a given path
 start.the.shit <- function(path = "~/Dropbox/IOW/R-functions/Particle_locator/test", ...){
     dirs <- list.dirs(path = path, full.names = TRUE)
     for (dir in dirs[-1]) { # to exclude the first directory which might always be 'path' itself ??
@@ -96,4 +112,4 @@ start.the.shit <- function(path = "~/Dropbox/IOW/R-functions/Particle_locator/te
 }
 
 
-start.the.shit(sep.window = T, firstlast = F)
+start.the.shit(sep.window = F, firstlast = F)
